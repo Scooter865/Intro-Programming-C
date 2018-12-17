44fb28fb18eaf2d9946207463d0120f2689b0fe2
Coursera Learner <nobody@nowhere.nul>
2018-12-17 00:44:23 +0000

    submit assignment 22

---
+++ 22_tests_power/test-power.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+unsigned power(unsigned x, unsigned y);
+
+int main(void){
+  unsigned x[] =   {0, 1, 2, 2, 2, 2, -2, -2,  2,     -2};
+  unsigned y[] =   {0, 1, 0, 1, 2, 3,  3,  4, -3,     -3};
+  unsigned ans[] = {1, 1, 1, 2, 4, 8, -8, 16,  0.125, -0.125};
+  size_t n = 10;
+  
+  //loop through each test case
+  for (int i = 0; i < n; i++) {
+    printf("power returned %d, ans is %d\n", power(x[i], y[i]), ans[i]);
+    if (power(x[i], y[i]) != ans[i]){
+      printf("Encountered wrong answer\n");
+      //exit if the returned value is incorrect
+      exit(EXIT_FAILURE);
+    }
+  }
+  printf("correct implementation!\n");
+  return EXIT_SUCCESS;
+}
