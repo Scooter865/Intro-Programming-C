afd739979272862240480eecf23b150452dc5748
Scooter865 <42327824+Scooter865@users.noreply.github.com>
2019-05-13 20:29:54 -0400

    Update outname.c

--- 29_outname/outname.c
+++ 29_outname/outname.c
@@ -4,5 +4,8 @@
 #include "outname.h"
 
 char * computeOutputFileName(const char * inputName) {
-  //WRITE ME
+  char * outName = malloc((strlen(inputName)+8) * sizeof(*outName));
+  strcpy(outName, inputName);
+  strcat(outName, ".counts");
+  return outName;
 }
