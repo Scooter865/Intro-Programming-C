ab813402d080f3caf5383d2d95351ce961633af9
Scooter865 <42327824+Scooter865@users.noreply.github.com>
2019-05-15 13:09:16 -0400

    Add files via upload

--- 30_sort_lines/sortLines.c
+++ 30_sort_lines/sortLines.c
@@ -16,9 +16,58 @@ void sortData(char ** data, size_t count) {
   qsort(data, count, sizeof(char *), stringOrder);
 }
 
+/*make functions for creating array, sorting array (done), and printing array
+since these steps are repeated if input is stdin or file(s)*/
+
+void doEverything(FILE * inputFile) {
+	//create the data array
+	char ** arrayOfStr = NULL;
+	char * line = NULL;
+  size_t lineSz = 0;
+  size_t nlines = 0;
+  while (getline(&line, &lineSz, inputFile) >= 0) {
+  	nLines++;
+  	arrayOfStr = realloc(arrayOfStr, nLines * sizeof(*arrayOfStr));
+  	arrayOfStr[nLines] = line;
+  	line = NULL;
+  }
+  free(line);
+
+  //sort the data
+  sortData(arrayOfStr, nLines);
+
+  //print and free each line
+  for (size_t i = 0; i < nLines; ++i) {
+  	printf("%s\n", arrayOfStr[i]);
+  	free(arrayOfStr[i]);
+  }
+  free(arrayOfStr);
+}
+
+
 int main(int argc, char ** argv) {
-  
-  //WRITE YOUR CODE HERE!
-  
+	if (argc == 1)	{
+		//no need to open or close any files, just create, sort, and print array
+		doEverything(stdin);
+	}
+	else if (argc > 1) {
+		//open file, create array, sort array, print array, close file
+		for (size_t i = 1; i < argc; ++i){
+			FILE * curFile = fopen(argv[1], "r");
+			if (curFile == NULL) {
+				fprintf(stderr, "problem openning file\n", );
+				return(EXIT_FAILURE);
+			}
+			doEverything(curFile);
+			if(fclose(curFile) != 0) {
+				fprintf(stderr, "problem closing file\n", );
+				return(EXIT_FAILURE);
+			}
+		}
+	}
+	else {
+		fprintf(stderr, "problem with inputs\n", );
+		return(EXIT_FAILURE);
+	}
   return EXIT_SUCCESS;
 }
