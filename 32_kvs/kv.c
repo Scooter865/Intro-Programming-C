848b5aa15d1bf4c3a4d2bff7476f9186dc2b46a7
Coursera Learner <nobody@nowhere.nul>
2019-07-10 22:27:06 +0000

    push back to github for dev

--- 32_kvs/kv.c
+++ 32_kvs/kv.c
@@ -4,106 +4,114 @@
 #include "kv.h"
 
 
-char * getKey(FILE * file) {
-	int k;
-	char * curKey;
-	size_t i = 0;
-	while((k = fgetc(file)) != '=') {
-		if (k == EOF) {
-			return NULL; //using NULL to indicate EOF
-		}
-		else {
-			curKey[i] = k;
-			i++;
-		}
-	}
-	return curKey;
-}
-
-char * getVal(FILE * file) {
-	int v;
-	char * curVal;
-	size_t i = 0;
-	while((v = fgetc(file)) != '\n') {
-		if (v == EOF) {
-			return NULL;
-		}
-		else {
-			curVal[i] = v;
-			i++;
-		}
-	}
-	return curVal;
+void getKey(FILE * file, char * curKey) {
+  int k;
+  size_t i = 0;
+  while((k = fgetc(file)) != '=') {
+    if (k == EOF) {
+      curKey = NULL; //using NULL to indicate EOF
+    }
+    else {
+      curKey = realloc(curKey, (i+1) * sizeof(char));
+      curKey[i] = k;
+      i++;
+    }
+  }
 }
 
 
-int writePair(FILE * file, kvarray_t * targetArray, size_t pairIdx) {
-	char * keyToWrite = getKey(file);
-	char * valToWrite = getVal(file);
+void getVal(FILE * file, char * curVal) {
+  int v;
+  size_t i = 0;
+  while((v = fgetc(file)) != '\n') {
+    if (v == EOF) {
+      curVal = NULL;
+    }
+    else {
+      curVal = realloc(curVal, (i+1) * sizeof(char));
+      curVal[i] = v;
+      i++;
+    }
+  }
+}
 
-	//indicate to calling function when EOF is encountered
-	if ((keyToWrite == NULL)||(valToWrite == NULL)) {
-		return 0;
-	}
 
-	//need to write duplicate check
+int writePair(FILE * file, kvarray_t * targetStruct, size_t pairIdx) {
+  char * curKey = malloc(sizeof(char));
+  char * curVal = malloc(sizeof(char));
+  getKey(file, curKey);
+  getVal(file, curVal);
+  
+  //indicate to calling function when EOF is encountered
+  if ((curKey == NULL)||(curVal == NULL)) {
+    return 0;
+  }
+  
+  //need to write duplicate check
 
-	(targetArray->nKVs)++;
-	targetArray = realloc(targetArray, sizeof(kvpair_t) + (nKVs * sizeof(kvpair_t)));
-	(targetArray->kvArray[pairIdx]).key = keyToWrite;
-	(targetArray->kvArray[pairIdx]).value = valToWrite;
-
-	//indicate to calling function that a pair was written
-	return 1;
+  targetStruct->nKVs = pairIdx + 1;
+  targetStruct = realloc(targetStruct, sizeof(kvpair_t) + (targetStruct->nKVs * sizeof(kvpair_t)));
+  (targetStruct->kvArray[pairIdx]).key = curKey;
+  (targetStruct->kvArray[pairIdx]).value = curVal;
+  
+  //indicate to calling function that a pair was written
+  return 1;
+  //Not free-ing anything here, free-ing to be done in freeKVs
 }
 
 
 kvarray_t * readKVs(const char * fname) {
-	//Open file
-	FILE * f = fopen(fname, "r");
-	if (f == NULL) {
-		fprintf(stderr, "Problem opening file\n");
-		exit(EXIT_FAILURE);
-	}
+  //Open file
+  FILE * f = fopen(fname, "r");
+  if (f == NULL) {
+    fprintf(stderr, "Problem opening file\n");
+    exit(EXIT_FAILURE);
+  }
 
-	//Allocate the array to write kv pairs to
-	kvarray_t * kvPairs = malloc(sizeof(kvarray_t));
-	kvPairs->nKVs = 0;
+  //Allocate the struct to write kv pairs to
+  kvarray_t * kvPairs = malloc(sizeof(kvarray_t));
+  kvPairs->nKVs = 0;
 
-	//Call funciton to write pairs as long as EOF hasn't been reached
-	int pairWritten = 1;
-	size_t pairNo = 0; //purposely not using nKVs for empty file corner case
-	do {
-		pairWritten = writePair(f, kvPairs, pairNo);
-		pairNo++;
-	}
-	while (pairWritten != 0);
-
-	//Close file
-	if(fclose(f) != 0) {
-		fprintf(stderr, "Problem closing file\n");
-	}
-
-	return kvPairs;
+  //Call funciton to write pairs as long as EOF hasn't been reached
+  int pairWritten = 1;
+  size_t pairNo = 0; //purposely not using nKVs for empty file corner case
+  do {
+    pairWritten = writePair(f, kvPairs, pairNo);
+    pairNo++;
+  }
+  while (pairWritten != 0);
+  
+  //Close file
+  if(fclose(f) != 0) {
+    fprintf(stderr, "Problem closing file\n");
+  }
+  return kvPairs;
 }
 
+
 void freeKVs(kvarray_t * pairs) {
-  free(pairs);
+  /*dig down into each kvpair_t element in pairs and free the key and value members.
+    All of these were allocated by multiple writePair calls*/
+  for (size_t i = 0; i < pairs->nKVs; i++) {
+    free(pairs->kvArray[i].key);
+    free(pairs->kvArray[i].value);
+  }
+  free(pairs); //The kvarray_t was also allocated
 }
 
 void printKVs(kvarray_t * pairs) {
-	for(size_t i = 0; i < pairs->nKVs; i++) {
-  		printf("key = '%s' value = '%s'\n", (pairs->kvArray[i]).key, (pairs->kvArray[i]).value);
-  	}
+  for(size_t i = 0; i < pairs->nKVs; i++) {
+    printf("key = '%s' value = '%s'\n", (pairs->kvArray[i]).key, (pairs->kvArray[i]).value);
+  }
 }
 
 
 /*Searches paris for key and returns corresponding value*/
 char * lookupValue(kvarray_t * pairs, const char * key) {
-	for (size_t i = 0; i < pairs->nKVs; i++) {
-		if ((strcmp(pairs->kvArray[i]).key, key) == 0) {
-			return ((pairs->kvArray[i]).value);
-		}
-	}
-	return NULL;
+  for (size_t i = 0; i < pairs->nKVs; i++) {
+    if (strcmp((pairs->kvArray[i]).key, key) == 0) {
+      return ((pairs->kvArray[i]).value);
+    }
+  }
+  return NULL;
 }
