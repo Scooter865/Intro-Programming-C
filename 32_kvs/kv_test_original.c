ea8dda9eb90081fd3d5692b63e6127d219fa9041
Coursera Learner <nobody@nowhere.nul>
2019-07-13 01:38:14 +0000

    push to github

--- 32_kvs/kv_test_original.c
+++ 32_kvs/kv_test_original.c
@@ -3,12 +3,12 @@
 #include <string.h>
 #include "kv.h"
 
-#define NUM_LOOKUPS 1
+#define NUM_LOOKUPS 5
 int main(void) {
   kvarray_t * array = readKVs("test.txt");
   printf("Printing all keys\n\n");
   printKVs(array);
-  char *tests[NUM_LOOKUPS] = {"banana"};
+  char *tests[NUM_LOOKUPS] = {"banana", "grapes", "cantaloupe", "lettuce", "orange"};
   for (int i = 0; i < NUM_LOOKUPS; i++) {
     printf("lookupValue('%s')=%s\n", tests[i], lookupValue(array,tests[i]));
   }
