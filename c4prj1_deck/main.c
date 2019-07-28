7486e62f79a312a4ba16d909a8aaac502a965b33
Scooter865 <42327824+Scooter865@users.noreply.github.com>
2019-07-28 16:13:34 -0400

    More function tests

--- c4prj1_deck/main.c
+++ c4prj1_deck/main.c
@@ -11,6 +11,7 @@ int main(void) {
   size_t lnsz = 0;
   card_t curCard;
   
+  printf("Enter 7 cards to make a hand\n");
   for (int i = 0; i < 7; i++) {
     getline(&cardStr, &lnsz, stdin);
     curCard = card_from_letters(cardStr[0], cardStr[1]);
@@ -19,7 +20,16 @@ int main(void) {
   
   printf("here's the hand:\n");
   print_hand(hand1);
+
+  printf("here's the rest of the deck\n");
+  deck_t * restOfDeck = make_deck_exclude(hand1);
+  print_hand(restOfDeck);
+
+  printf("here's the hand with an empty card added\n");
+  add_empty_card(hand1);
+  print_hand(hand1);
   
   free_deck(hand1);
+  free_deck(restOfDeck);
   return EXIT_SUCCESS;
 }
