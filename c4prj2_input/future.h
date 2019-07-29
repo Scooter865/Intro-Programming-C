0f8b17e3f2c1c73930e4812f11aaf0393740d861
Scooter865 <42327824+Scooter865@users.noreply.github.com>
2019-07-29 16:12:08 -0400

    future.h formatting change

--- c4prj2_input/future.h
+++ c4prj2_input/future.h
@@ -8,8 +8,8 @@ struct future_cards_tag {
   size_t n_decks;
 };
 typedef struct future_cards_tag future_cards_t;
+
 void add_future_card(future_cards_t * fc, size_t index, card_t * ptr) ;
 void future_cards_from_deck(deck_t * deck, future_cards_t * fc);
-
 
 #endif 
