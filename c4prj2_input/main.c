693f419d06646404914ebbae8800ed6d605a3769
Coursera Learner <nobody@nowhere.nul>
2019-08-06 17:51:00 +0000

    submit for grade

--- c4prj2_input/main.c
+++ c4prj2_input/main.c
@@ -16,21 +16,21 @@ int main(int argc, char ** argv) {
   fc->decks = NULL;
 
   deck_t ** hands = read_input(f, &n_hands, fc);
-  printf("Here are the input hands:\n");
+  /*printf("Here are the input hands:\n");
   for (size_t i = 0; i < n_hands; i++) {
     print_hand(hands[i]);
-  }
+    }*/
 
   deck_t * deck = build_remaining_deck(hands, n_hands);
-  printf("Here's the remaining deck:\n");
-  print_hand(deck);
+  /*printf("Here's the remaining deck:\n");
+    print_hand(deck);*/
   shuffle(deck);
 
   future_cards_from_deck(deck, fc);
-  printf("Here are the filled in hands:\n");
+  /*printf("Here are the filled in hands:\n");
   for (size_t i = 0; i < n_hands; i++) {
     print_hand(hands[i]);
-  }
+    }*/
 
   free_deck(deck);
   for (size_t i = 0; i < n_hands; i++) {
