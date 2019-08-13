3f1cdb4bb6a832d51c6f5e301ddbc141f9bb4ead
Scooter865 <42327824+Scooter865@users.noreply.github.com>
2019-08-13 09:12:54 -0400

    Make main.c

--- c4prj3_finish/main.c
+++ c4prj3_finish/main.c
@@ -11,6 +11,82 @@
 
 
 int main(int argc, char ** argv) {
-  //YOUR CODE GOES HERE
+	int num_trials;
+
+	if (argc < 2) {
+		fprintf(stderr, "Need more inputs\n");
+  	exit(EXIT_FAILURE);
+	}
+	else if (argc == 3) {
+		num_trials = argv[2];
+	}
+	else {
+		num_trials = 10000;
+	}
+
+  FILE * input = fopen(argv[1]);
+  if (input == NULL) {
+  	fprintf(stderr, "Problem opening file\n");
+  	exit(EXIT_FAILURE);
+  }
+
+  future_cards_t * fc = malloc(sizeof(future_cards_t));
+  fc->n_decks = 0;
+  fc->decks = NULL;
+  size_t n_hands = 0;
+
+  deck_t ** hands = read_input(input, &n_hands, fc);
+  printf("Here are the input hands:\n");
+  for (size_t i = 0; i < n_hands; i++) {
+    print_hand(hands[i]);
+  }
+
+  deck_t * remainingDeck = build_remaining_deck(hands, n_hands); //Should maybe allocate this
+  printf("Here's the remaining deck:\n");
+  print_hand(deck);
+
+  int windex[n_hands + 1];
+  for (size_t i = 0; i < n_hands; i++) {
+  	windex[i] = 0;
+  }
+
+  int compRes;
+  size_t winnerIdx;
+
+  for (size_t i = 0; i < num_trials; i++) {
+    shuffle(remainingDeck);
+    future_cards_from_deck(remainingDeck, fc);
+    winnerIdx = 0;
+
+    for (size_t j = 1; j < n_hands; j++) {
+   		compRes = compare_hands(hands[winnerIdx], hands[j])
+   		/*if (compRes > 0) {
+   			No action keep the winner index where it is
+   		}*/
+   		if (compRes == 0) {
+   			windex[n_hands]++;
+   		}
+   		else if (compRes < 0) {
+   			winnerIdx = j;
+   		}
+   	}
+   	windex[winnerIdx]++;
+  }
+
+  for (size_t i = 0; i < n_hands; i++) {
+  	printf("Hand %zu won %u / %u times (%.2f%%)\n", i, windex[i], num_trials, (windex[i]/num_trials));
+  }
+  printf("And there were %u ties\n", windex[n_hands]);
+
+  free_deck(remainingDeck);
+  for (size_t i = 0; i < n_hands; i++) {
+    free_deck(hands[i]);
+  }
+  free(hands);
+  for (size_t i = 0; i < fc->n_decks; i++) {
+    free_deck(&fc->decks[i]);
+  }
+  free(fc);
+
   return EXIT_SUCCESS;
 }
